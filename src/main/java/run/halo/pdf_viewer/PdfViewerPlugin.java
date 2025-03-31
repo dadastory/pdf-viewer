55088bb84f515031edc0f101189a4138fe2d2ddb
chenda <441730067@qq.com>
2025-03-31 23:54:24 +0800

    # 初始化项目改造， 准备支持PDF组件

--- src/main/java/run/halo/pdf_viewer/PdfViewerPlugin.java
+++ src/main/java/run/halo/pdf_viewer/PdfViewerPlugin.java
@@ -1,4 +1,4 @@
-package run.halo.starter;
+package run.halo.pdf_viewer;
 
 import org.springframework.stereotype.Component;
 import run.halo.app.plugin.BasePlugin;
@@ -13,9 +13,9 @@ import run.halo.app.plugin.PluginContext;
  * @since 1.0.0
  */
 @Component
-public class StarterPlugin extends BasePlugin {
+public class PdfViewerPlugin extends BasePlugin {
 
-    public StarterPlugin(PluginContext pluginContext) {
+    public PdfViewerPlugin(PluginContext pluginContext) {
         super(pluginContext);
     }
 
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,12 +1,19 @@
-import { fileURLToPath, URL } from "url";
+import {fileURLToPath, URL} from "url";
 
-import { HaloUIPluginBundlerKit } from "@halo-dev/ui-plugin-bundler-kit";
+import {HaloUIPluginBundlerKit} from "@halo-dev/ui-plugin-bundler-kit";
+import path from "path";
+import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+import {defineConfig, Plugin} from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Icons from "unplugin-icons/vite";
-import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [Vue(), Icons({ compiler: "vue3" }), HaloUIPluginBundlerKit()],
+  plugins: [Vue(), Icons({compiler: "vue3"}),
+    HaloUIPluginBundlerKit(),
+    VueI18nPlugin({
+      include: [path.resolve(__dirname, "./src/locales/*.yaml")],
+    }) as Plugin,
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
